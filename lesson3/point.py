0036aa7b9d13285cb8e90c1e19c742946027e5cc
choccocookie <166429989+choccocookie@users.noreply.github.com>
2024-05-18 21:47:19 +0300

    Update point.py

--- lesson3/point.py
+++ lesson3/point.py
@@ -9,7 +9,20 @@
 
 
 class Point:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def __str__(self):
+        return f"{self.x}, {self.y}"
+
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return self.x, self.y
 
 
 # код для проверки 
